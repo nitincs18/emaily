25fa8dae02e951febe755d99b3b03cc2d0a04118
Nitin Kumar <knitin472@gmail.com>
2019-09-03 17:30:50 +0530

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+
+
+module.exports={
+    googleClientID:'311862843748-4s534nvtao79ofsh3bs24r7n7g4rkv5v.apps.googleusercontent.com',
+    googleClientSecret:'d9k2z4N-ESsFqhKad52mDIe8',
+    mongoURI:'mongodb://sa:ab_TAK56@ds225253.mlab.com:25253/node-react',
+    cookieKey: 'vyetfwgdkhsntvhdfgbhnmhjbnkghbgvnhkj'
+}
